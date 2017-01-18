9651ee9dc51bb017a2fc81c643679a74893ee7a5
first <hiroakishimohira@hiroakinoMacBook-Pro.local>
2017-01-18 11:15:10 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+user_csv = CSV.readlines("db/users_view.csv")
+user_csv.shift
+user_csv.each do |row|
+  User.create(name: row[1], email: row[2], encrypted_password: row[3])
+end
